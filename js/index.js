5eb978c371f7880c6968f0e0fa675dee4d3ae9ba
Tom Harris <tom_3rd_2000@yahoo.com>
2020-08-09 19:05:21 -0400

    had to start over messing around with git commands, js/index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,73 @@
 // Your code goes here
+// 1  On mouseover
+const myHover = document.querySelector(".logo-heading");
+myHover.addEventListener("mouseover", () => {
+    myHover.style.color = "pink";
+    });
+
+// 2 On mouseout
+myHover.addEventListener("mouseout", () => {
+    myHover.style.color = "black";
+    });
+
+// 3 On Click
+const myNav = document.querySelector(".logo-heading");
+myNav.addEventListener("click", () => {
+    alert("Thanks for clicking");
+});
+
+// 4 On load
+
+// eslint-disable-next-line no-unused-vars
+const body = document.querySelector("body");
+
+window.addEventListener("load", () => {
+   alert("Hey there!  My Name is Tommy");
+ });
+
+// 5 On wheel
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+  
+    // Restrict scale
+    scale = Math.min(Math.max(.125, scale), 4);
+  
+    // Apply scale transform
+    el.style.transform = `scale(${scale})`;
+  }
+  
+  let scale = 1;
+  const el = document.querySelector('img');
+  el.onwheel = zoom;
+
+// 6 dblclick
+const footerClick = document.querySelector('.footer');
+
+
+footerClick.addEventListener('dblclick', function () {
+  alert("want to change font size")
+});
+
+// 7 on Focus / 8 on Blur
+const form = document.getElementById('form');
+
+form.addEventListener('focus', (event) => {
+  event.target.style.background = 'pink';    
+}, true);
+
+form.addEventListener('blur', (event) => {
+  event.target.style.background = '';    
+}, true);
+
+// 9 Adding .stopPropagation, and .preventDefault to all of my .nav links
+const navLinks = document.querySelectorAll(".main-navigation .nav-container .nav .nav-link");
+
+navLinks.forEach(link => link.addEventListener("click", (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    navLinks.forEach(link => link.style.textDecoration = "none")
+    event.target.style.textDecoration = "underline";
+    
+}));
